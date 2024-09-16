f54aca77a56f05664a840ab1fe9f188dff04ee1e
ingridmartins <ingridvazmartins@gmail.com>
2024-09-16 15:53:46 +0100

    Basic crud

--- src/br/com/collections/list/ListaDeTarefas.java
+++ src/br/com/collections/list/ListaDeTarefas.java
@@ -32,4 +32,16 @@ public class ListaDeTarefas {
     public void obterDescricaoTarefas(){
         System.out.println(listaDeTarefas);
     }
+
+    public static void main(String[] args) {
+        ListaDeTarefas listaDeTarefas = new ListaDeTarefas();
+        System.out.println("O numero total de tarefas é: " + listaDeTarefas.obterNumeroTotalTarefas());
+        listaDeTarefas.adicionarTarefa("Tarefa 1");
+        listaDeTarefas.adicionarTarefa("Tarefa 2");
+        System.out.println("O numero total de tarefas é: " + listaDeTarefas.obterNumeroTotalTarefas());
+        listaDeTarefas.removerTarefa("Tarefa 1");
+        System.out.println("O numero total de tarefas é: " + listaDeTarefas.obterNumeroTotalTarefas());
+        listaDeTarefas.obterDescricaoTarefas();
+
+    }
 }
